c1deb35dd797cda5b47d01ee83b57d1431e29d43
Mahdi BolandGerami <93255641+mahdibg98@users.noreply.github.com>
2021-11-15 18:56:00 +0330

    Create adb_ex1.sql

---
+++ sql/adb_ex1.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 15, 2021 at 03:10 PM
+-- Server version: 5.7.31
+-- PHP Version: 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `adb_ex1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `user id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `family` varchar(200) NOT NULL,
+  `birthdate` varchar(200) NOT NULL,
+  `tel` varchar(200) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `gender` varchar(200) NOT NULL,
+  PRIMARY KEY (`user id`)
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user id`, `name`, `family`, `birthdate`, `tel`, `address`, `gender`) VALUES
+(1, 'mahdi', 'bolandgerami', '1998-06-26', '09033208877', 'Iran/Hamedan', 'male'),
+(2, 'zahra', 'bolandgerami', '2006-02-08', '09123456789', 'iran/hamedan', 'female');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
