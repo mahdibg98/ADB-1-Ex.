d904c9db28b648994b4ff10d56a54d93eff43556
Mahdi BolandGerami <93255641+mahdibg98@users.noreply.github.com>
2021-11-15 19:29:23 +0330

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
 <?php
     $user = "root";
-	$pass = "";
+    $pass = "";
 
-	try{
-		$databaseconnect = new PDO( "mysql:host=localhost;dbname=adb_ex1" , $user , $pass );
-		$databaseconnect->exec("set names utf8");
-	}
-	catch(PDOException $e){
-		header("Location: error.php");
-	}
+    try{
+	$databaseconnect = new PDO( "mysql:host=localhost;dbname=adb_ex2" , $user , $pass );
+	$databaseconnect->exec("set names utf8");
+    }
+    catch(PDOException $e){
+	header("Location: error.php");
+    }
 ?>
